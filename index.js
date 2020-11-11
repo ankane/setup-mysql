59e1cf0db96e1df719dda1c81974cd6586f6c753
Andrew Kane <andrew@chartkick.com>
2020-11-11 03:21:22 -0800

    Updated order

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ if (process.platform == 'darwin') {
   if (mysqlVersion != '8.0') {
     // install
     run(`wget -q https://dev.mysql.com/get/mysql-apt-config_0.8.16-1_all.deb`);
-    run(`sudo apt install ./mysql-apt-config_0.8.16-1_all.deb`);
     run(`echo mysql-apt-config mysql-apt-config/enable-repo select mysql-${mysqlVersion} | sudo debconf-set-selections`);
-    run(`sudo dpkg --configure -a`);
+    run(`sudo apt install ./mysql-apt-config_0.8.16-1_all.deb`);
+    // run(`sudo dpkg --configure -a`);
     run(`sudo apt-get install mysql-server-${mysqlVersion}`);
   }
 
