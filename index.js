37caffa0db675f7d591b99a9e6e33cd86a755247
Andrew Kane <andrew@chartkick.com>
2020-11-11 03:38:07 -0800

    Try another version

--- index.js
+++ index.js
@@ -28,10 +28,6 @@ if (process.platform == 'darwin') {
     run(`sudo rm -r /var/lib/mysql`);
 
     // install new version
-    run(`wget -q https://dev.mysql.com/get/mysql-apt-config_0.8.16-1_all.deb`);
-    run(`echo mysql-apt-config mysql-apt-config/select-server mysql-${mysqlVersion} | sudo debconf-set-selections`);
-    run(`sudo dpkg -i mysql-apt-config_0.8.16-1_all.deb`);
-    run(`cat /etc/apt/sources.list.d/mysql.list`);
     run(`sudo apt-get install mysql-server-${mysqlVersion}`);
   }
 
