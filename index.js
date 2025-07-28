70c3f5972819728443852d1a131125790061e7c6
Andrew Kane <andrew@ankane.org>
2025-07-27 22:33:04 -0700

    Improved Homebrew config [skip ci]

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ function run() {
   const args = Array.from(arguments);
   console.log(args.map(v => v.toString().includes(' ') ? `"${v}"` : v).join(' '));
   const command = args.shift();
+  let env = Object.assign({}, process.env);
+  env.HOMEBREW_NO_AUTO_UPDATE = '1';
+  env.HOMEBREW_NO_INSTALL_CLEANUP = '1';
   // spawn is safer and more lightweight than exec
-  const ret = spawnSync(command, args, {stdio: 'inherit'});
+  const ret = spawnSync(command, args, {stdio: 'inherit', env: env});
   if (ret.status !== 0) {
     throw ret.error;
   }
@@ -18,9 +21,7 @@ function run() {
 
 function runUnsafe(command) {
   console.log(command);
-  let env = Object.assign({}, process.env);
-  delete env.CI; // for Homebrew on macos-11.0
-  execSync(command, {stdio: 'inherit', env: env});
+  execSync(command, {stdio: 'inherit'});
 }
 
 function addToPath(newPath) {
